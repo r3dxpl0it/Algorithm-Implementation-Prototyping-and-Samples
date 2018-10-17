d6399c0c78c17a87ca0a35ef1eb6a70780816f9d
Jim Was Here <43002597+r3dxpl0it@users.noreply.github.com>
2018-10-17 13:47:51 +0200

    Update BASIC_LIN_REG_SKLEARN.py

--- machine-learning/sklearn/BASIC_LIN_REG_SKLEARN.py
+++ machine-learning/sklearn/BASIC_LIN_REG_SKLEARN.py
@@ -45,3 +45,57 @@ class Linear_Regression():
 	def return_clf(self):
 		self.Apply()
 		return self.clf
+
+	
+	
+	
+#Another Version that i have Written for Another Test : 
+#OLDER CODE :
+#
+'''
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+def SINGLE_AND_MULTIPLE_LINEAR_REGRESSION_BASIC(X_TRAINER , Y_TRAINER , X_TESTER  , Y_TESTER ,
+                        return_Table = False  , N_JOBS = 1):
+	#Fit simple Linear Model with n_jobs
+	Regs = LinearRegression(n_jobs=N_JOBS)
+	Regs.fit(X_TRAINER , Y_TRAINER)
+	#Predicting The Test_Set Results
+	if return_Table == True:
+		return 	Regs.predict(X_TESTER) , Regs.score(X_TESTER)
+
+def SINGLE_LINEAR_REGRESSION_VPLUS(X_TRAINER , Y_TRAINER , X_TESTER  , Y_TESTER ,
+					  visulizing_mode = False ,  return_Table = False  , N_JOBS = -1 ,
+                       TITLE = None , X_lab= None , Y_lab = None , Show = None):
+	#import Lib
+	#Fit simple Linear Model with n_jobs
+	Regs = LinearRegression(n_jobs=N_JOBS)
+	Regs.fit(X_Train , Y_Train)
+	#Predicting The Test_Set Results
+	Y_Pred = Regs.predict(X_Test)
+	if return_Table == True:
+		return 	Regs.predict(X_TESTER) , Regs.score(X_TESTER)
+	#Visualizing
+	if visulizing_mode == True :
+        plt.scatter(X_Train , Y_Train , color ='red')
+        plt.scatter(X_Test , Y_Test , color = 'blue' )
+        plt.plot(X_Train ,Regs.predict(X_Train) , color = 'green')
+        if TITLE != None :
+            plt.title(TITLE)
+        if X_lab != None :
+            plt.xlabel(X_lab)
+        if Y_lab != None :
+            plt.ylabel(Y_lab)
+        if Show != None :
+            plt.show()
+
+
+#ACC , PRED = SINGLE_AND_MULTIPLE_LINEAR_REGRESSION_BASIC(X_train , y_train , x_test , y_test)
+#
+#Regressor = LinearRegression()
+#Regressor.fit(TRAIN_SET_X , TRAIN_SET_Y)
+#Regressor.fit(TEST_SET_X)
+#Regressor.score(TEST_SET_X , TEST_SET_Y)
+'''
+	
