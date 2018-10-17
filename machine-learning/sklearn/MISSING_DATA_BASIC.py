cd35fc302525985ed0981df25578cff8a703fee7
Jim Was Here <43002597+r3dxpl0it@users.noreply.github.com>
2018-10-17 13:38:43 +0200

    Create MISSING_DATA_BASIC.py

---
+++ machine-learning/sklearn/MISSING_DATA_BASIC.py
@@ -0,0 +1,22 @@
+from sklearn.preprocessing import Imputer
+
+def MISSING_DATA_BASIC(init_data , start_col , end_col , Missing_Val = 'NaN' ,  Strategy = 'median'):
+	imputer = Imputer(missing_values= Missing_Val , strategy = Strategy)
+	imputer = imputer.fit(init_data[:,start_col:end_col+1])
+	init_data[:,start_col:end_col+1] = imputer.transform(init_data[:,start_col:end_col+1])
+
+
+'''
+#INDIRECT USAGE :
+
+#MISSING VALUE PREDICTION (COL WIS)
+MISSING_DATA_BASIC(X , start_col=1 , end_col=2)
+
+
+#DIRECT USAGE :
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, FROM:TO])
+X[:, FROM:TO] = imputer.transform(X[:, FROM:TO])
+'''
