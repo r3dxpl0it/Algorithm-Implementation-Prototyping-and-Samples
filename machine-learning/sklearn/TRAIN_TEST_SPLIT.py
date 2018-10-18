5232cdad1fcb847bd1749f3b67e26d563d718ec9
Jim Was Here <43002597+r3dxpl0it@users.noreply.github.com>
2018-10-18 22:45:24 +0200

    Update TRAIN_TEST_SPLIT.py

--- machine-learning/sklearn/TRAIN_TEST_SPLIT.py
+++ machine-learning/sklearn/TRAIN_TEST_SPLIT.py
@@ -1,9 +1,20 @@
+'''_____________________________________________________________________
+|[] R3DXPL0IT SHELL                                            |ROOT]|!"|
+|"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""|"| 
+|CODED BY > R3DXPLOIT(JIMMY)                                          | |
+|EMAIL > RETURN_ROOT@PROTONMAIL.COM                                   | |
+|GITHUB > https://github.com/r3dxpl0it                                | |
+|WEB-PAGE > https://r3dxpl0it.Github.io                               |_|
+|_____________________________________________________________________|/|
+'''
+try:
+	from sklearn.cross_validation import train_test_split
+except DeprecationWarning as e:
+	print (e , "NEED TO BE CHECKED !")
+from sklearn.model_selection import train_test_split
+
 def TRAIN_TEST_SPLIT(X_var , Y_var , TEST_SIZE = 20 , RAND_STATE = None):
-	try:
-		from sklearn.cross_validation import train_test_split
-	except DeprecationWarning as e:
-		print (e , "NEED TO BE CHECKED !")
-		from sklearn.model_selection import train_test_split
+
 	return train_test_split(X_var , Y_var , test_size = TEST_SIZE/100 , random_state = RAND_STATE)
 
 '''#DATA SPLITING IN 2 GROUPS (TEST/TRAIN):
