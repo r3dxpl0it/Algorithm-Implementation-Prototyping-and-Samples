0ae76da9f7aa65b62520b4ce43eb68659e7ed0e9
Jim Was Here [a.k.a R3dxpl0it] <return_root@protonmail.com>
2019-02-08 15:18:02 +0100

    Update sample.py

--- MumboJumbo/src/sample.py
+++ MumboJumbo/src/sample.py
@@ -1,4 +1,4 @@
-from Mumbo import MumboJumbo
+from MumboJumbo import MumboJumbo
 Test = MumboJumbo(function=MumboJumbo.Benchmarks.Schwefel, minimization= True ,
                   initialization_bound=[-500 , 500] , termination = {'max_gen' : 1000 , 'max_n' : 0.001}  ,
                   initialization_sizing_method =  'differenziale' , dim = 100 , verbose=True , debug= False)
